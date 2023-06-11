f785f7e79438f2e227964ccbc177d90a0c47e0a8
jp-96 <jp-96@outlook.jp>
2023-06-11 05:32:08 +0000

    current

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  LatLng currentLatLng = LatLng(35.681, 139.767);
+  LatLng myLocationLatLng = LatLng(35.681, 139.767);
   MapController mapController = MapController();
 
-  @override
-  void initState() {
-    super.initState();
-    initLocation();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   refreshMyLocation();
+  // }
 
-  Future<void> initLocation() async {
+  Future<void> refreshMyLocation() async {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
@@ -39,21 +39,29 @@ class _MyAppState extends State<MyApp> {
     final latitude = position.latitude;
     final longitude = position.longitude;
     setState(() {
-      currentLatLng = LatLng(latitude, longitude);
+      myLocationLatLng = LatLng(latitude, longitude);
     });
-    mapController.move(currentLatLng, 18);
+    mapController.move(myLocationLatLng, 18);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("openstreetmap Flutter"),
+        title: const Text("gsi Flutter"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.my_location,
+        ),
+        onPressed: () {
+          refreshMyLocation();
+        },
       ),
       body: FlutterMap(
         mapController: mapController,
         options: MapOptions(
-          center: currentLatLng,
+          center: myLocationLatLng,
           zoom: 15.0,
           maxZoom: 18,
           minZoom: 9,
@@ -62,15 +70,16 @@ class _MyAppState extends State<MyApp> {
         children: [
           TileLayer(
             urlTemplate:
-                "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
-            //urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-            //userAgentPackageName: 'land_place',
+                // "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                // "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
+                "https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
+            userAgentPackageName: "land_place",
           ),
           MarkerLayer(markers: [
             Marker(
                 width: 30.0,
                 height: 30.0,
-                point: currentLatLng,
+                point: myLocationLatLng,
                 builder: (ctx) => const Icon(
                       Icons.location_on,
                       color: Colors.blueAccent,
@@ -78,24 +87,6 @@ class _MyAppState extends State<MyApp> {
                     ))
           ]),
         ],
-        // layers: [
-        //   TileLayerOptions(
-        //       urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
-        //   MarkerLayerOptions(markers: [
-        //     Marker(
-        //         width: 30.0,
-        //         height: 30.0,
-        //         point: LatLng(-12.069783, -77.034057),
-        //         builder: (ctx) => Container(
-        //                 child: Container(
-        //               child: Icon(
-        //                 Icons.location_on,
-        //                 color: Colors.blueAccent,
-        //                 size: 40,
-        //               ),
-        //             )))
-        //   ])
-        // ],
       ),
     );
   }
